6eb53a2f87bbf56278f1606d2fb791d90fd546ca
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-05-30 08:44:16 -0300

    criação do css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import './App.css';
+
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 className="title">Hello world</h1>
     </div>
   );
 }
